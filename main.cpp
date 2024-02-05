33aa0d92e678c38d3d222e3ae79471ede1e4daad
Sahuf Shaikh <127684377+sahuf2003@users.noreply.github.com>
2024-02-05 07:40:20 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int x=10;
+    int *p = &x;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    cout<<&x<<endl;
+
+
+    return 0;
+}
